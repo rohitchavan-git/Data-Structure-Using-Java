c0f905c5cdb2e394d3f2472389f1ccfa66c4a508
rohit <rohit.chavan@dhdigital.co.in>
2020-02-02 23:13:48 -0900

    add reverse linked list funtionality
    add reverse linked list funtionality

--- DataStructureUsingJava/src/linkedList/SLListMain.java
+++ DataStructureUsingJava/src/linkedList/SLListMain.java
@@ -3,18 +3,21 @@ package linkedList;
 public class SLListMain {
 
 	public static void main(String[] args) {
-			
-		SLList slList=new SLList();
+
+		SLList slList = new SLList();
 		slList.add(10);
 		slList.add(11);
 		slList.add(12);
 		slList.add(13);
-		slList.add(14);
-		slList.add(15);
-		slList.add(16);
-		slList.add(17);
-		slList.add(18);
 		slList.showList();
+		System.out.println("is 15 present in list :" + slList.search(10));
+		System.out.println("Reverse Linked List");
+		slList.revList();
+		slList.showList();
+
+		
+		System.out.println("get at 3 index =>"+slList.get(2));
+		
 		
 	}
 
